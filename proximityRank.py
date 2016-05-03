d29d8cf639b15ebf0d71ea0568aee2549bc63b51
Li Sida <davidli43@hotmail.com>
2016-05-03 10:55:59 -0500

    modification to calculate 2nd and 3rd order adjacency matrix

--- proximityRank.py
+++ proximityRank.py
@@ -36,7 +36,7 @@ numPaper = len(papers)
 #               ... ...
 #            ]
 
-citationCount1stOrder = np.zeros(numPaper)
+citationCount2ndOrder = np.zeros(numPaper)
 
 count = -1
 f = open("pub_out_paperid_citpaper.txt", 'r')
@@ -47,7 +47,7 @@ for line in f:
     else:
         citation.append([items[0], items[1].rstrip('\r\n')])
         count += 1
-    citationCount1stOrder[count] += 1
+    citationCount2ndOrder[count] += 1
 
 # W_first and W_second are adjacency matrices, both #papers by # papers
 # W_first[i][j] = 1 if the ith paper is cited or cites the jth paper
@@ -63,15 +63,29 @@ for item in citation:
 
 if os.path.exists("W_second.npy"):
     W_second = np.load('W_second.npy')
-
 else:
     # calculate number of paths between two papers on second order
     # then set to 1 if there is at least one path 
     # np.dot(W_first, W_first_T, W_second)
     W_first_T = W_first.transpose()
     W_second = fastdot.dot(W_first, W_first_T)
-
+    np.fill_diagonal(W_second, 0.0)
     np.save('W_second.npy', W_second)
+
+# calculate W_second till this line
+
+if os.path.exists("W_third.npy"):
+    W_second = np.load('W_third.npy')
+else:
+    # calculate number of paths between two papers on second order
+    # then set to 1 if there is at least one path 
+    # np.dot(W_first, W_first_T, W_second)
+    W_second_T = W_second.transpose()
+    W_third = fastdot.dot(W_second, W_second_T)
+    np.fill_diagonal(W_third, 0.0)
+    np.save('W_third.npy', W_second)
+
+
 
 
 # calculate number of paths between two papers on third order, commented out to reduce calculation
@@ -81,26 +95,28 @@ else:
 # for testing purposes, we chood to calculate simularity for the 4th paper, i.e. paper[3]
 # note that some papers, for example, paper[0], it doesn't have any 1st order connections due to the sparsity of our citation network
 # paper[0] has citations, but are not listed in our set of papers
-queryIndex = 3
+queryIndex = idHash['473736']
 paperUnion = []
 for i in range(numPaper):
-    paperUnion.append( citationCount1stOrder[queryIndex] + citationCount1stOrder[i] - W_first[queryIndex][i])
+    paperUnion.append( citationCount2ndOrder[queryIndex] + citationCount2ndOrder[i] - W_first[queryIndex][i])
 
-similarity1stOrder = W_first[queryIndex]/paperUnion
+similarity2ndOrder = W_first[queryIndex]/paperUnion
 
 #TODO: rank similarity1stOrder
 
 #2nd order citation count, if A cites B which cites C, then A has one 2nd order citation count
-citationCount2ndOrder = np.dot(W_first, citationCount1stOrder) - citationCount1stOrder
 paperUnion = []
 for i in range(numPaper):
     paperUnion.append( citationCount2ndOrder[queryIndex] + citationCount2ndOrder[i] - W_second[queryIndex][i])
 
 similarity2nOrder = W_second[queryIndex]/paperUnion
 
+
+# citationCount3rdOrder = np.dot(W_first, citationCount1stOrder) - citationCount1stOrder
+
 PaperScores = []
 for i in range(numPaper):
-    PaperScores.append((similarity2nOrder[i],i))
+    PaperScores.append((similarity2ndOrder[i],i))
 
 sortedPaperScores = sorted(PaperScores, key = itemgetter(0), reverse = True)
 print "Sorted paper scores for paper: " + papers[queryIndex][1]
